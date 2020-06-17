10c0648603b20c7a25329d0e0efbd07220c3d433
Mark Cram <mark.cram@oracle.com>
2020-06-18 08:08:29 +1000

    Use container_registry for load balancer olcne-nginx
    Signed-off-by: Mark Cram <mark.cram@oracle.com>

--- modules/olcne/scripts/create_kubernetes_module.template.sh
+++ modules/olcne/scripts/create_kubernetes_module.template.sh
@@ -8,6 +8,7 @@ echo "Creating Kubernetes module ${cluster_name} in environment ${environment}"
 olcnectl --api-server 127.0.0.1:8091 module create --environment-name "${environment}" \
   --module kubernetes --name "${cluster_name}" \
   --container-registry "${container_registry}"/olcne \
+  --nginx-image "${container_registry}"/olcne/nginx:1.12.2 \
   --virtual-ip "${master_vip}" \
   --master-nodes "${master_nodes_addresses}" \
   --worker-nodes "${worker_nodes_addresses}" 2> /dev/null
